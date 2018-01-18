29017ba21b3890e97ed5a5247b96ac3499aea123
AnatolSich <tip-77@ukr.net>
2018-01-18 12:24:13 +0200

    Adapter added

---
+++ Adapter/AdapterTest.java
@@ -0,0 +1,65 @@
+package Adapter;
+
+public class AdapterTest {
+    public static void main(String[] args) {
+        RoundHole roundHole1 = new RoundHole(25);
+
+        RoundPeg roundPeg1 = new RoundPeg(15);
+        RoundPeg roundPeg2 = new RoundPeg(30);
+
+        Fit.checkFit(roundHole1, roundPeg1);
+        Fit.checkFit(roundHole1, roundPeg2);
+
+        SquarePeg squarePeg1 = new SquarePeg(10, 20);
+        SquarePeg squarePeg2 = new SquarePeg(30, 10);
+        AdapterPeg adapterPeg1 = new AdapterPeg(squarePeg1);
+        AdapterPeg adapterPeg2 = new AdapterPeg(squarePeg2);
+        AdapterPPeg adapterPPeg1 = new AdapterPPeg(squarePeg1);
+        AdapterPPeg adapterPPeg2 = new AdapterPPeg(squarePeg2);
+
+        Fit.checkFit(roundHole1, adapterPeg1);
+        Fit.checkFit(roundHole1, adapterPeg2);
+        Fit.checkFit(roundHole1, adapterPPeg1);
+        Fit.checkFit(roundHole1, adapterPPeg2);
+    }
+}
+
+class Fit {
+    static void checkFit(Round roundHole1, Round roundPeg2) {
+        if (roundHole1.getRadius() >= roundPeg2.getRadius()) {
+            System.out.println("Ok");
+        } else System.out.println("No");
+    }
+}
+//Адаптер на основе композиции
+class AdapterPeg extends RoundPeg {
+    SquarePeg squarePeg = null;
+
+    public AdapterPeg(SquarePeg squarePeg) {
+        super(getMaxDimension(squarePeg));
+        this.squarePeg = squarePeg;
+    }
+
+    static int getMaxDimension(SquarePeg squarePeg) {
+        int a = squarePeg.getX();
+        int b = squarePeg.getY();
+        if (a >= b) return a;
+        else return b;
+    }
+}
+//Адаптер на основе множественного наследования
+class AdapterPPeg extends SquarePeg implements Round {
+
+
+    public AdapterPPeg(SquarePeg squarePeg) {
+        super(squarePeg.getX(), squarePeg.getY());
+    }
+
+    @Override
+    public int getRadius() {
+        int a = this.getX();
+        int b = this.getY();
+        if (a >= b) return a;
+        else return b;
+    }
+}
---
+++ Adapter/Round.java
@@ -0,0 +1,49 @@
+package Adapter;
+
+public interface Round {
+    int getRadius();
+}
+
+class RoundHole implements Round {
+    int r;
+
+    public RoundHole(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public int getRadius() {
+        return this.r;
+    }
+}
+
+class RoundPeg implements Round {
+    int r;
+
+    public RoundPeg(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public int getRadius() {
+        return this.r;
+    }
+}
+
+class SquarePeg {
+    int x;
+    int y;
+
+    public SquarePeg(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
